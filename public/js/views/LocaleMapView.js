bc86d5a88ccf63eecad6d675b8ba549c15b73d14
JakeCooper <Jake.elijah.cooper@gmail.com>
2015-03-15 02:58:12 -0700

    Added auto pop on sidr after one sec.

--- public/js/views/LocaleMapView.js
+++ public/js/views/LocaleMapView.js
@@ -82,7 +82,6 @@ define([
 
 			      var pos = new google.maps.LatLng(position.coords.latitude,
 			                                       position.coords.longitude);
-
 			     var marker = new google.maps.Marker({
 				      position: pos,
 				      map: Map,
@@ -101,7 +100,6 @@ define([
 				    	+ '</div>' 
 				    	+ '<div class="waypoint-info-dismiss"><i class="fa fa-angle-up fa-lg"></i></div>'
 			     })*/
-
 	      		Map.setCenter(pos);
 			});
 		},
