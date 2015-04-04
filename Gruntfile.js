3babeb5f409cdf4984f9ffd138943d0048d95cfa
Chris Hampu <chrishampu@outlook.com>
2015-04-04 01:57:29 -0700

    Deploy minified JS files if applicable.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
-var path = require('path');
-var async = require('async');
+var path = require('path'),
+	async = require('async'),
+	fs    = require('fs'),
+    bower = require('bower');
 
 module.exports = function(grunt) {
 
@@ -19,10 +21,59 @@ module.exports = function(grunt) {
 					'frontend/css/style.css': 'frontend/css/style.scss'
 				}
 			}
+		},
+		copy: {
+			deploy: {
+				files: [
+					{ flatten: true, src: ['bower_components/backbone/backbone.js'], dest: 'deploy/js/libs/backbone/backbone.js', filter: 'isFile'  },
+					{ flatten: true, src: ['bower_components/requirejs/require.js'], dest: 'deploy/js/libs/require/require.js', filter: 'isFile'  },
+					{ flatten: true, src: ['bower_components/requirejs-hbs/hbs.js'], dest: 'deploy/js/libs/requirejs-hbs/hbs.js', filter: 'isFile'  },
+					{ flatten: true, src: ['bower_components/jquery/jquery.js'], dest: 'deploy/js/libs/jquery/dist/jquery.js', filter: 'isFile'  },
+					{ flatten: true, src: ['bower_components/underscore/underscore.js'], dest: 'deploy/js/libs/underscore/underscore-min.js', filter: 'isFile'  },
+					{ flatten: true, src: ['bower_components/thorax/thorax.js'], dest: 'deploy/js/libs/thorax/thorax.min.js', filter: 'isFile'  },
+					{ flatten: true, src: ['bower_components/handlebars/handlebars.js'], dest: 'deploy/js/libs/handlebars/handlebars.js', filter: 'isFile'  },
+					{ flatten: true, src: ['bower_components/text/text.js'], dest: 'deploy/js/libs/text/text.js', filter: 'isFile' },
+					{ flatten: true, src: ['bower_components/bootstrap/dist/bootstrap.js'], dest: 'deploy/js/libs/bootstrap/bootstrap.min.js', filter: 'isFile'  },
+					{ flatten: true, src: ['frontend/js/libs/sidr/jquery.sidr.min.js'], dest: 'deploy/js/libs/sidr/jquery.sidr.min.js', filter: 'isFile' }
+				]
+			},
+			dev: {
+				files: [
+					{ flatten: true, src: ['bower_components/backbone/backbone.js'], dest: 'frontend/js/libs/backbone/backbone.js', filter: 'isFile'  },
+					{ flatten: true, src: ['bower_components/requirejs/require.js'], dest: 'frontend/js/libs/require/require.js', filter: 'isFile'  },
+					{ flatten: true, src: ['bower_components/requirejs-hbs/hbs.js'], dest: 'frontend/js/libs/requirejs-hbs/hbs.js', filter: 'isFile'  },
+					{ flatten: true, src: ['bower_components/jquery/jquery.js'], dest: 'frontend/js/libs/jquery/dist/jquery.min.js', filter: 'isFile'  },
+					{ flatten: true, src: ['bower_components/underscore/underscore.js'], dest: 'frontend/js/libs/underscore/underscore.js', filter: 'isFile'  },
+					{ flatten: true, src: ['bower_components/thorax/thorax.js'], dest: 'frontend/js/libs/thorax/thorax.js', filter: 'isFile'  },
+					{ flatten: true, src: ['bower_components/handlebars/handlebars.js'], dest: 'frontend/js/libs/handlebars/handlebars.js', filter: 'isFile'  },
+					{ flatten: true, src: ['bower_components/text/text.js'], dest: 'frontend/js/libs/text/text.js', filter: 'isFile' },
+					{ flatten: true, src: ['bower_components/bootstrap/dist/bootstrap.js'], dest: 'frontend/js/libs/bootstrap/bootstrap.js', filter: 'isFile'  }
+				]
+			}
 		}
 	});
 	
 	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	
+	grunt.registerTask('ensure-installed', function() {
+		var complete = this.async();
+		
+		if (!fs.existsSync(__dirname + "/bower_components")) {
+			bower.commands.install().on('data', function(data) {
+					process.stdout.write(data);
+			}).on('error', function(data) {
+					process.stderr.write(data);
+			}).on('end', function (data) {
+				if (data) {
+					process.stdout.write(data);
+				}
+				complete();
+			});
+		} else {
+			complete();
+		}
+	});	
 	
 	grunt.registerTask('server', 'Run backend server', function() {
 		var done = this.async();
@@ -37,9 +88,11 @@ module.exports = function(grunt) {
 	
 	grunt.registerTask('deploy', 'Create production build', function() {
 		grunt.task.run('sass:deploy');
+		grunt.task.run('copy:deploy');
 	});
 	
 	grunt.registerTask('default', function() {
-	
+		grunt.task.run('ensure-installed');
+		grunt.task.run('copy:dev');
 	});
 };
